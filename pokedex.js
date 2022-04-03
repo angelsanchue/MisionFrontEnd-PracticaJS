08e23a0fccba9ba2467a3719ea08ba0b74e51995
Angel <44126119+angelsanchue@users.noreply.github.com>
2022-04-03 08:34:28 -0500

    Inicial

---
+++ pokedex.js
@@ -0,0 +1,51 @@
+const fetchPokemon = () => {
+    const inputPokemon = document.getElementById("txtPokemon").value.toLowerCase();
+    const url = 'https://pokeapi.co/api/v2/pokemon/'+inputPokemon;
+    
+    fetch(url).then((res)=> {
+        if(res.status == 404){
+            cambiarImagenNoEncontrado("assets/pikachu_llorando.gif")
+            cambiarTitulo("Pokemon no encontrado");
+            //console.log("es 404");
+        }
+        
+
+        console.log(res);
+        return res.json();
+
+    }).then((data)=> {
+
+        
+
+        cambiarTitulo(data.name);
+        cambiarImagen(data.sprites.front_default);
+        agregarDatos(data);
+        console.log(data);  
+    })
+}
+
+const cambiarImagen = (url)=>{
+    document.getElementById("pokemonImg").src = url;
+    document.getElementById("pokemonImg").width = "170";
+}
+
+const cambiarImagenNoEncontrado = (url)=>{
+    document.getElementById("pokemonImg").src = url;
+    document.getElementById("pokemonImg").width = "130";
+}
+
+const cambiarTitulo = (title)=>{
+    
+    document.getElementById("namePokemon").innerHTML = title;
+}
+
+const agregarDatos = (data)=>{
+
+    document.getElementById("txtTipo").innerHTML = "Tipo: "+data.types[0].type.name;
+    document.getElementById("txtPoder").innerHTML = "Poder: "+data.stats[0].base_stat;
+    document.getElementById("txtAtaque").innerHTML = "Ataque: "+data.stats[1].base_stat;
+    document.getElementById("txtDefensa").innerHTML = "Defensa: "+data.stats[2].base_stat;
+    document.getElementById("txtAtaqueS").innerHTML = "Ataque especial: "+data.stats[3].base_stat;
+    document.getElementById("txtDefensaS").innerHTML = "Defensa especial: "+data.stats[4].base_stat;
+    document.getElementById("txtVelocidad").innerHTML = "Velocidad: "+data.stats[5].base_stat;
+}
